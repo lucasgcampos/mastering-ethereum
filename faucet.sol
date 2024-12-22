270f309abf04e0b3ff7eb4b70e4e618e1bc733a0
Lucas Campos <lucasgcampos.contato@gmail.com>
2024-12-21 23:04:02 -0300

    Faucet contract

---
+++ faucet.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Faucet {
+
+    function withdraw(uint withdraw_amount) external {
+        require(withdraw_amount <= 1 ether);
+        payable(msg.sender).transfer(withdraw_amount);
+    }
+
+    function balance() view external returns(uint) {
+        return address(this).balance;
+    }
+}
