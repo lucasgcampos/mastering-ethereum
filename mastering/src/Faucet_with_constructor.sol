a17a6d1fda2d6b6e5a8cf5f47c9706d2fbbe674d
Lucas Campos <lucasgcampos.contato@gmail.com>
2025-01-03 22:00:47 -0300

    Faucet with constructor

---
+++ mastering/src/Faucet_with_constructor.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Faucet {
+
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function withdraw(uint withdraw_amount) external {
+        require(withdraw_amount <= 0.1 ether);
+        payable(msg.sender).transfer(withdraw_amount);
+    }
+
+    function balance() view external returns(uint) {
+        return address(this).balance;
+    }
+
+    receive() external payable {}
+}
