0305e67bbdb4ec4bc0319918132d62996ebcfda7
Lucas Campos <lucasgcampos.contato@gmail.com>
2025-01-12 22:59:43 -0300

    Default visibility solution

---
+++ mastering/src/09_default_visibilities/HashForEtherProtected.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract HashForEther { 
+
+    function withdrawWinnings() external {
+        require(uint32(uint160(msg.sender)) == 0);
+        _sendWinnings();
+    }
+
+    function _sendWinnings() private {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
