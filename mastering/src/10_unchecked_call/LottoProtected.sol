1c43be6e8294afbbc6d113d9611b510988d7b66b
Lucas Campos <lucasgcampos.contato@gmail.com>
2025-01-13 00:06:26 -0300

    Unchecked call response solution

---
+++ mastering/src/10_unchecked_call/LottoProtected.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Lotto {
+    bool public payedOut = false;
+    address public winner;
+    uint public winAmount;
+
+    function sendToWinner() public {
+        require(!payedOut);
+        (bool response, ) = payable(winner).send(winAmount);
+        require(response);
+        payedOut = true;
+    }
+
+    function withdrawLeftOver() public {
+        require(payedOut);
+        payable(msg.sender).send(address(this).balance);
+    }
+}
