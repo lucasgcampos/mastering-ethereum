7641095eb68b9801c745db76448bfebbac4b2224
Lucas Campos <lucasgcampos.contato@gmail.com>
2025-01-14 20:47:14 -0300

    ERC20 openZeppelin

--- mastering/src/11_ERC20/METoken.sol
+++ mastering/src/11_ERC20/METoken.sol
@@ -6,6 +6,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract METoken is ERC20 {
     
     constructor() ERC20("Mastering Ethereum Token", "MET") {
-       
+        uint256 initialSupply = 21000000 * 10 ** uint256(decimals());
+        _mint(msg.sender, initialSupply); 
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 2;
     }
 }
